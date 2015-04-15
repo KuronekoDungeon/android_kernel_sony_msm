c13f6392c4aa0eb5ca15c2c5a99d35f56e48f86c
Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
2015-04-15 16:16:12 -0700

    UPSTREAM: zsmalloc: remove synchronize_rcu from zs_compact()
    (cherry-pick from commit 1ec7cfb13acb8047ae5baafb43d2cd6b64ac85b9)
    
    Do not synchronize rcu in zs_compact(). Neither zsmalloc not
    zram use rcu.
    
    Bug: 25951511
    
    Change-Id: I2f2d1a81dac561ddfabb861bedcbb1ba773f207f
    Signed-off-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Acked-by: Minchan Kim <minchan@kernel.org>
    Cc: Nitin Gupta <ngupta@vflare.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/zsmalloc.c
+++ mm/zsmalloc.c
@@ -1752,8 +1752,6 @@ unsigned long zs_compact(struct zs_pool *pool)
 		nr_migrated += __zs_compact(pool, class);
 	}
 
-	synchronize_rcu();
-
 	return nr_migrated;
 }
 EXPORT_SYMBOL_GPL(zs_compact);
