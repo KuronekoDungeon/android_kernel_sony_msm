1ff1aa5e81b17ada8236d9b61ceec7db355cbc59
Ezekeel <notezekeel@googlemail.com>
2015-07-06 22:59:52 +0800

    cpufreq: wheatley: bring up!
    Signed-off-by: Levin Calado <levincalado@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -467,12 +467,16 @@ extern struct cpufreq_governor cpufreq_gov_conservativex;
 extern struct cpufreq_governor cpufreq_gov_smartmax;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartmax)
 <<<<<<< HEAD
+<<<<<<< HEAD
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)
 extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_pegasusq)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKZACTIVE)
 extern struct cpufreq_governor cpufreq_gov_yankzactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankzactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_WHEATLEY)
+extern struct cpufreq_governor cpufreq_gov_wheatley;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_wheatley)
 #endif
 
 /*********************************************************************
