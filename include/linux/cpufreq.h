91f7a9638d575b7503c91944213d1c547bec4e3e
DespairFactor <matthewdalex92@gmail.com>
2016-03-14 01:42:08 -0400

    cpufreq: add despair governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -475,6 +475,9 @@ extern struct cpufreq_governor cpufreq_gov_smartmax;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)
 extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_pegasusq)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DESPAIR)
+extern struct cpufreq_governor cpufreq_gov_despair;
+#define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_despair)
 #endif
 
 /*********************************************************************
