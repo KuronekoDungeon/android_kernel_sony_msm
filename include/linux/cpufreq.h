6e257ea23a016897f2afcb387cbaa423ec9052fa
maz-1 <ohmygod19993@gmail.com>
2016-06-23 12:46:44 +0800

    cpufreq : add bluactive and conservativex

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -454,6 +454,12 @@ extern struct cpufreq_governor cpufreq_gov_thunderx;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BIOSHOCK)
 extern struct cpufreq_governor cpufreq_gov_bioshock;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_bioshock)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLUACTIVE)
+extern struct cpufreq_governor cpufreq_gov_bluactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_bluactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CONSERVATIVEX)
+extern struct cpufreq_governor cpufreq_gov_conservativex;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_conservativex)
 #endif
 
 /*********************************************************************
