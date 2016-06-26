c26fae7bba8e0ca8b4e076f215310197ef3497e6
maz-1 <ohmygod19993@gmail.com>
2016-06-26 10:47:22 +0800

    fix cherry-pick problems

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -466,8 +466,6 @@ extern struct cpufreq_governor cpufreq_gov_conservativex;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
 extern struct cpufreq_governor cpufreq_gov_smartmax;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartmax)
-<<<<<<< HEAD
-<<<<<<< HEAD
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)
 extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_pegasusq)
