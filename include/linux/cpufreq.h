8860821dbe7d4a12d63426cfd558cbc9cdf3f563
maz-1 <ohmygod19993@gmail.com>
2016-07-01 16:51:12 +0800

    Revert "Revert "cpufreq: add impulse""
    This reverts commit 64ce895250db9d622bc4df64f888a1fa92fa2991.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -457,6 +457,9 @@ extern struct cpufreq_governor cpufreq_gov_smartmax_eüs;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
 extern struct cpufreq_governor cpufreq_gov_alucard;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_alucard)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
+extern struct cpufreq_governor cpufreq_gov_impulse;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
 extern struct cpufreq_governor cpufreq_gov_zzmoove;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_zzmoove)
